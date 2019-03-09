bfe898419fb3f2426da74649fbb743958f3b4dc1
Ismail <cucevicismail@gmail.com>
2019-03-09 17:17:24 +0100

    save

--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -2,7 +2,7 @@ const maxChar = require('./index');
 
 test('maxChar function exists', () => {
   expect(typeof maxChar).toEqual('function');
-});
+}); 
 
 test('Finds the most frequently used char', () => {
   expect(maxChar('a')).toEqual('a');
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,45 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
 
-module.exports = reverse;
+   function reverse(str){
+    let reversed = '';
+    
+    for(let character of str) {
+        reversed = character + reversed;
+        debugger;
+    }
+ 
+    return reversed;
+ }
+ 
+reverse('asff')
+
+module.exports = reverse; 
+
+
+//1) function reverse(str) {
+//      const arr = str.split('');
+//      arr.reverse();    
+//      return arr.join('');
+// }
+ 
+
+//2) function reverse(str){
+//     let reversed = '';
+//     console.log(str);
+//     for(let character of str) {
+//         reversed = character + reversed;
+//     }
+//     console.log(reversed); 
+ 
+//     return reversed;
+//  }
+
+// function reverse(str){
+//     debugger;
+//    return str.split('').reduce((reversed, character) => {
+//       return character + reversed;
+//    }, '');
+// }
+ 
